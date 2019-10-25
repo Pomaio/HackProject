6978df55cc7aea6eb0085d540df71bf3c0206f43
grish <grisha_marshinov@mail.ru>
2019-10-25 23:39:10 +0300

    add test page

--- src/app/pages/page-routing/page-routing.module.ts
+++ src/app/pages/page-routing/page-routing.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {PageRoutingComponent} from './page-routing.component';
 import {ProfileComponent} from '../profile/profile.component';
+import { TestComponent } from '../test/test.component';
 
 
 const routes: Routes = [
@@ -10,8 +11,9 @@ const routes: Routes = [
     component: PageRoutingComponent,
     children: [
       {path: 'profile', component: ProfileComponent},
+      {path: 'test', component: TestComponent}
     ]
-  }
+  },
 ];
 
 @NgModule({
