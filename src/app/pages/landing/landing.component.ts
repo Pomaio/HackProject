921f9e1f0e018092360e836f4404a3bf21ba4f8d
Vladimir Gaydukov <vovavovaa1a@gmail.com>
2019-10-26 02:25:47 +0300

    some fix

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {fromEvent, Subject} from 'rxjs';
+import {takeUntil, throttleTime} from 'rxjs/operators';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.scss']
 })
-export class LandingComponent implements OnInit {
-
-  constructor() { }
+export class LandingComponent implements OnInit, OnDestroy {
+  moveEvent$;
+  countX = 0;
+  countY = 0;
+  destroy$ = new Subject();
 
   ngOnInit() {
+    this.moveEvent$ = fromEvent(document.getElementById('page__container'), 'mousemove');
+    this.moveEvent$.pipe(
+      throttleTime(2),
+      takeUntil(this.destroy$)
+    ).subscribe((e) => {
+      this.countX += e.movementX / 5;
+      this.countY += e.movementY / 5;
+      document.getElementById('page__container').style.backgroundPositionX = `${this.countX}px`;
+      document.getElementById('page__container').style.backgroundPositionY = `${this.countY}px`;
+    });
   }
 
+  ngOnDestroy() {
+    this.destroy$.next();
+  }
 }
