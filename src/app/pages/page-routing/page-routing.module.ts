6983aeaf3b18bd0f6233bcfe501b2749297c6ef1
Shchurbin Egor <rentgen.94@yandex.ru>
2019-10-26 02:34:16 +0300

    map skills ready

--- src/app/pages/page-routing/page-routing.module.ts
+++ src/app/pages/page-routing/page-routing.module.ts
@@ -3,27 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { PageRoutingComponent } from './page-routing.component';
 import { ProfileComponent } from '../profile/profile.component';
 import { TestComponent } from '../test/test.component';
-<<<<<<< HEAD
-import {LandingComponent} from '../landing/landing.component';
-
-=======
+import { LandingComponent } from '../landing/landing.component';
 import { MapComponent } from '../map/map.component';
->>>>>>> map skills ready
 
 const routes: Routes = [
   {
     path: '',
     component: PageRoutingComponent,
     children: [
-<<<<<<< HEAD
-      {path: 'profile', component: ProfileComponent},
-      {path: 'landing', component: LandingComponent},
-      {path: 'test', component: TestComponent}
-=======
+      { path: 'landing', component: LandingComponent },
       { path: 'profile', component: ProfileComponent },
       { path: 'test', component: TestComponent },
       { path: 'map', component: MapComponent }
->>>>>>> map skills ready
     ]
   }
 ];
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {ProfileComponent} from './profile/profile.component';
-import {PageRoutingModule} from './page-routing/page-routing.module';
-import {PageRoutingComponent} from './page-routing/page-routing.component';
-import {HeaderModule} from '../components/header/header.module';
-import {FooterModule} from '../components/footer/footer.module';
-import {TestComponent} from './test/test.component';
-import {ReactiveFormsModule} from '@angular/forms';
-import {MatButtonModule, MatFormFieldModule, MatInputModule, MatStepperModule} from '@angular/material';
-import {FormModule} from './test/form/form.module';
-import { LandingComponent } from './landing/landing.component';
-
-=======
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProfileComponent } from './profile/profile.component';
@@ -24,8 +9,8 @@ import { TestComponent } from './test/test.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule, MatFormFieldModule, MatInputModule, MatStepperModule } from '@angular/material';
 import { FormModule } from './test/form/form.module';
+import { LandingComponent } from './landing/landing.component';
 import { MapModule } from './map/map.module';
->>>>>>> map skills ready
 
 @NgModule({
   declarations: [ProfileComponent, TestComponent, PageRoutingComponent, LandingComponent],
