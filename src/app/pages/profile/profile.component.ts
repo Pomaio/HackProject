aab22c0561526aa82871da198eb6ed3dd66c5857
Shchurbin Egor <rentgen.94@yandex.ru>
2019-10-26 02:58:27 +0300

    Add materialize js

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import * as M from 'materialize-css/dist/js/materialize.min';
+
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent implements OnInit, AfterViewInit {
+  @ViewChild('carousel', { static: true }) carousel: ElementRef;
 
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  ngAfterViewInit() {
+    const instances = M.Carousel.init(this.carousel.nativeElement, null);
   }
-
 }
