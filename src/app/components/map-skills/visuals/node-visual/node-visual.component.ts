4e1cf6a7ec74fd86dcd351931ffb0479f746df83
Shchurbin Egor <rentgen.94@yandex.ru>
2019-10-26 10:27:05 +0300

    Merge test service

--- src/app/components/map-skills/visuals/node-visual/node-visual.component.ts
+++ src/app/components/map-skills/visuals/node-visual/node-visual.component.ts
@@ -1,10 +1,15 @@
 import { Component, Input } from '@angular/core';
 import { EducationNode } from 'src/app/_models';
+import { Router } from '@angular/router';
 
 @Component({
   selector: '[appNodeVisual]',
   template: `
-    <svg:g [attr.transform]="'translate(' + node.x + ',' + node.y + ')'">
+    <svg:g
+      [attr.transform]="'translate(' + node.x + ',' + node.y + ')'"
+      (dblclick)="onDoubleClick($event)"
+      matTooltip="Кликните два раза для перехода на урок."
+    >
       <svg:circle class="node" [attr.fill]="node.color" cx="0" cy="0" [attr.r]="node.r"></svg:circle>
       <svg:text class="node-name" [attr.font-size]="node.fontSize">
         {{ node.id }}
@@ -15,4 +20,11 @@ import { EducationNode } from 'src/app/_models';
 })
 export class NodeVisualComponent {
   @Input('appNodeVisual') node: EducationNode;
+
+  constructor(public router: Router) {}
+
+  onDoubleClick(event: any) {
+    event.stopPropagation();
+    this.router.navigate(['/test/' + this.node.chalengeId]);
+  }
 }
--- src/app/pages/test/test.component.ts
+++ src/app/pages/test/test.component.ts
@@ -1,5 +1,10 @@
-import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ChallengeService } from 'src/app/_services/challenge.service';
+import { Challenge } from 'src/app/_models';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-test',
@@ -8,25 +13,25 @@ import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 })
 export class TestComponent implements OnInit {
   isLinear = false;
+  challenge$: Observable<Challenge>;
 
-
-  constructor(private _formBuilder: FormBuilder) {
-  }
+  constructor(
+    private _formBuilder: FormBuilder,
+    public challengeService: ChallengeService,
+    private route: ActivatedRoute
+  ) {}
 
   profileForm = new FormGroup({
-    math: new FormControl('', [
-      Validators.required,
-    ]),
-    rush: new FormControl('', [
-      Validators.required,
-    ]),
-    physic: new FormControl('', [
-      Validators.required,
-    ]),
+    math: new FormControl('', [Validators.required]),
+    rush: new FormControl('', [Validators.required]),
+    physic: new FormControl('', [Validators.required])
   });
 
-  ngOnInit() {}
-
+  ngOnInit() {
+    this.challenge$ = this.route.paramMap.pipe(
+      switchMap((params: ParamMap) => this.challengeService.getChallengeById(Number.parseInt(params.get('id'), 10)))
+    );
+  }
 
   onSubmit() {
     console.warn(this.profileForm.value);
